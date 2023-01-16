e9986d2ca68c5f8e319af6fa6f9b01b872ea2c3a
Dariusz Hryciuk <you@example.com>
2023-01-16 18:38:34 +0100

    add lombok

--- src/main/java/com/example/model/Student.java
+++ src/main/java/com/example/model/Student.java
@@ -1,10 +1,17 @@
 package com.example.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "student")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
 
     @Id
@@ -16,34 +23,34 @@ public class Student {
     private String firstname;
 
 
-    public Student(){ }
-
-    public Student(@NotNull String firstname) {
-        this.firstname = firstname;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstnameName() {
-        return firstname;
-    }
-
-    public void setFirstnameName(String firstname) {
-        this.firstname = firstname;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + firstname + '\'' +
-                '}';
-    }
+//    public Student(){ }
+//
+//    public Student(@NotNull String firstname) {
+//        this.firstname = firstname;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getFirstnameName() {
+//        return firstname;
+//    }
+//
+//    public void setFirstnameName(String firstname) {
+//        this.firstname = firstname;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Student{" +
+//                "id=" + id +
+//                ", name='" + firstname + '\'' +
+//                '}';
+//    }
 
 }
